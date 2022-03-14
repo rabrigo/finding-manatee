3455c0dedcc44d57722477bf4b730a4adb6e748f
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-03-14 13:33:05 -0700

    latest update

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,25 +3,47 @@ import Card from './Card';
 
 function Board() {
     const [cards, setCards] = useState([
-        { id: 1, img: '/images/manatee.png', status: ""},
-        { id: 1, img: '/images/manatee.png', status: ""},
-        { id: 2, img: '/images/whale.png', status: ""},
-        { id: 2, img: '/images/whale.png', status: ""},
-        { id: 3, img: '/images/jellyfish.png', status: ""},
-        { id: 3, img: '/images/jellyfish.png', status: ""},
-        { id: 4, img: '/images/starfish.png', status: ""},
-        { id: 4, img: '/images/starfish.png', status: ""},
-        { id: 5, img: '/images/shark.png', status: ""},
-        { id: 5, img: '/images/shark.png', status: ""},
-        { id: 6, img: '/images/coral.jpeg', status: ""},
-        { id: 6, img: '/images/coral.jpeg', status: ""},
-        { id: 7, img: '/images/octopus.png', status: ""},
-        { id: 7, img: '/images/octopus.png', status: ""},
-        { id: 8, img: '/images/shrimp.png', status: ""},
-        { id: 8, img: '/images/shrimp.png', status: ""},
+        { id: 1, img: '/images/manatee.png', status: "" },
+        { id: 1, img: '/images/manatee.png', status: "" },
+        { id: 2, img: '/images/whale.png', status: "" },
+        { id: 2, img: '/images/whale.png', status: "" },
+        { id: 3, img: '/images/jellyfish.png', status: "" },
+        { id: 3, img: '/images/jellyfish.png', status: "" },
+        { id: 4, img: '/images/starfish.png', status: "" },
+        { id: 4, img: '/images/starfish.png', status: "" },
+        { id: 5, img: '/images/shark.png', status: "" },
+        { id: 5, img: '/images/shark.png', status: "" },
+        { id: 6, img: '/images/coral.jpeg', status: "" },
+        { id: 6, img: '/images/coral.jpeg', status: "" },
+        { id: 7, img: '/images/octopus.png', status: "" },
+        { id: 7, img: '/images/octopus.png', status: "" },
+        { id: 8, img: '/images/shrimp.png', status: "" },
+        { id: 8, img: '/images/shrimp.png', status: "" }
     ].sort(() => Math.random() - 0.5))
 
+    // function Board() {
+    //     const [cards, setCards] = useState([
+    //         { value: 'manatee', img: '/images/manatee.png', status: ""},
+    //         { value: 'manatee', img: '/images/manatee.png', status: ""},
+    //         { value: 'whale', img: '/images/whale.png', status: ""},
+    //         { value: 'whale', img: '/images/whale.png', status: ""},
+    //         { value: 'jellyfish', img: '/images/jellyfish.png', status: ""},
+    //         { value: 'jellyfish', img: '/images/jellyfish.png', status: ""},
+    //         { value: 'starfish', img: '/images/starfish.png', status: ""},
+    //         { value: 'starfish', img: '/images/starfish.png', status: ""},
+    //         { value: 'shark', img: '/images/shark.png', status: ""},
+    //         { value: 'shark', img: '/images/shark.png', status: ""},
+    //         { value: 'coral', img: '/images/coral.jpeg', status: ""},
+    //         { value: 'coral', img: '/images/coral.jpeg', status: ""},
+    //         { value: 'octopus', img: '/images/octopus.png', status: ""},
+    //         { value: 'octopus', img: '/images/octopus.png', status: ""},
+    //         { value: 'shrimp', img: '/images/shrimp.png', status: ""},
+    //         { value: 'shrimp', img: '/images/shrimp.png', status: ""},
+    //     ].sort(() => Math.random() - 0.5))
+
+    // when previous is -1 then no card id is the previous id
     const [previous, setPrevious] = useState(-1);
+    const [cardsPressed, setCardsPressed] = useState(0);
 
     const checkCard = (current) => {
         if(cards[current].id == cards[previous].id) {
@@ -29,6 +51,7 @@ function Board() {
             cards[previous].status = "correct";
             setCards([...cards]);
             setPrevious(-1);
+            setCardsPressed(0);
         } else {
             cards[current].status = "incorrect";
             cards[previous].status = "incorrect";
@@ -38,17 +61,30 @@ function Board() {
                 cards[previous].status = "";
                 setCards([...cards]);
                 setPrevious(-1);
+                setCardsPressed(0);
             }, 1000)
         }
     }
 
     const handleClick = (id) => {
-        if (previous === -1) {
-            cards[id].status = "active";
-            setCards([...cards]);
-            setPrevious(id);
-        } else {
-            checkCard(id);
+        if (cardsPressed < 2) {
+            if (previous === -1) {
+                if (cards[id].status !== "correct"){
+                    setCardsPressed(cardsPressed + 1);
+                    setPrevious(id);
+                    cards[id].status = "active";
+                    setCards([...cards]);
+                }
+            } else {
+                if (id !== previous)
+                {
+                    if (cards[id].status !== "correct")
+                    {
+                        setCardsPressed(cardsPressed + 1);
+                        checkCard(id);
+                    }
+                }
+            }
         }
     }
 
